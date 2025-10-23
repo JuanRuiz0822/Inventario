2daf512ae81c49fdcf7dd1b88ab788ed1444cdb5
Reaper2719 <jdiazr2704@gmail.com>
2025-10-23 12:23:03 -0500

    New 1.4 Limpieza de repositorio archivos innecesarios

--- iniciar_servidor.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Iniciando Sistema Inventario SENA..." -ForegroundColor Green
-Set-Location backend
-if (Test-Path .venv/Scripts/Activate.ps1) {
-    .venv/Scripts/Activate.ps1
-}
-python -m uvicorn app:app --reload --host 0.0.0.0 --port 8000
--- iniciar_servidor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Iniciando Sistema Inventario SENA..."
-cd backend
-if [ -d ".venv" ]; then
-    source .venv/bin/activate
-fi
-python -m uvicorn app:app --reload --host 0.0.0.0 --port 8000
